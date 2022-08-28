750797baa6eb3c21d2e2a382018a8ddc8f672ce5
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-08-28 02:04:54 +0100

    init

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/org/seo_authority-20220817010518.org" "/home/user/org/skyscraper_technique-20220817004708.org" "/home/user/org/qubes_salt-20201201033218.org" "/home/user/org/lisp-20220510224241.org" "/home/user/org/todos.org")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ emacs.el
@@ -0,0 +1,3 @@
+;;; ../.dotfiles/doom/.doom.d/emacs.el -*- lexical-binding: t; -*-
+
+(map! :desc "completion-at-point" "<C-SPC>" #'completion-at-point)
