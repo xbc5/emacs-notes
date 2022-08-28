506af0dc066b4e59cadaa5d48d115adc2d109853
xbc5 <87829033+xbc5@users.noreply.github.com>
2022-08-28 04:39:56 +0100

    refactor: remove unused rust mode

--- config.el
+++ config.el
@@ -2,7 +2,6 @@
 
 (load! "group/global.el")
 (load! "group/plantuml.el")
-(load! "group/rust.el")
 (load! "group/spell.el")
 (load! "group/general.el")
 (load! "group/org.el")
--- group/rust.el
+++
@@ -1,6 +0,0 @@
-;;; ../projects/linux/emacs-notes/group/rust.el -*- lexical-binding: t; -*-
-
-(use-package! rust-mode
-              :hook (rust-mode-hook . (setq indent-tabs-mode nil))
-              :config
-              (setq rust-format-on-save t))
