eb84002e5425393da180d20d386af6d098f321c8
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-06-11 04:57:41 +0100

    refactor: tidy up spell config

--- conf/spell.el
+++ conf/spell.el
@@ -1,24 +1,17 @@
-;;; ../projects/linux/emacs-notes/group/spell.el -*- lexical-binding: t; -*-
-
-;;- see: https://www.emacswiki.org/emacs/InteractiveSpell
-;; - frontends
-;;   - flyspell: handles /highlighting/
-;;     - uses hunspell|aspell|ispell backend
-;;   - ispell: handles /interactive/ spell-checking
-;;     - uses hunspell|aspell|ispell backend
-;;   - spell-fu: does highlighting, and correct on selection
-;;     - BUG: disabled, because words are neve un-highlighted
-;; - backends
+;; frontends
+;;   - flyspell: handles highlighting;
+;;   - ispell: (builtin) handles interactive spell-checking;
+;; backends
 ;;   - aspell: slow, but focuses on quality of suggestions
 ;;
-(after! flyspell
-  (setq flyspell-issue-message-flag nil)) ;; emits a message for each word, causes huge slowdown
+;; both flyspell and ispell can use hunspell, aspell, ispell as backens.
 
-(add-hook 'text-mode-hook 'flyspell-mode)       ;; for text files
-(add-hook 'prog-mode-hook 'flyspell-prog-mode)  ;; for source code comments
+(use-package! flyspell
+  :config (setq flyspell-issue-message-flag nil) ; emits a message for each word, causes huge slowdown
+  :hook ((text-mode . flyspell-mode) ; normal spell check
+         (prog-mode . flyspell-prog-mode))) ; for source code
 
-;; ispell is built-in, 'after! ispell' appears to not work
-(setq ispell-dictionary "british")
-;; FIXME: use nil, but ispell for some reason falls back to ~/.emacs.d/.local/...
-(setq ispell-personal-dictionary "~/.aspell.en.pws")  ;; nil means use the backend checker's default
-(setq ispell-program-name "aspell")  ;; it is the backend of ispell
+; builtin
+(setq ispell-dictionary "british"
+      ispell-personal-dictionary "~/.aspell.en.pws" ; nil means use the backend checker's default
+      ispell-program-name "aspell") ; set backend
