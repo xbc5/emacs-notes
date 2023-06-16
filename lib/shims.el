7927de9c9923f4549d3972cf20642290c3c77ac5
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-06-16 01:07:01 +0100

    feat: implement fuzzy node conflict resolver
    Squashed commit of the following:
    
    commit f4eccada95071704f2c802df3875ddd678f9e886
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri Jun 16 01:06:15 2023 +0100
    
        add doc string; this closes #6
    
    commit f9910d7863a59d69fb5a579f8dc9e4e55ab5780a
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Fri Jun 16 01:02:50 2023 +0100
    
        implement the fuzzy finder, use a shim as advice

--- lib/shims.el
+++ lib/shims.el
@@ -17,7 +17,33 @@
 
 (after! org-roam
   ;; batch all SQL operations as a single transaction (fixes slow file saves).
-  (advice-add 'org-roam-db-update-file :around
+  (advice-add 'org-roam-db-update-file
+              :around
               (defun +org-roam-db-update-file (fn &rest args)
                 (emacsql-with-transaction (org-roam-db)
                   (apply fn args)))))
+
+;; When saving, Roam will resolve roam:links. When there's a conflict,
+;;  this will prompt with a fuzzy finder to resolve each one.
+(defun +org-roam-node-from-title-or-alias (s)
+  "Prompt the user to select a node on conflict."
+  (let ((matches (seq-uniq
+                  (append
+                   (org-roam-db-query [:select [id] :from nodes
+                                       :where (= title $s1)]
+                                      s)
+                   (org-roam-db-query [:select [node-id] :from aliases
+                                       :where (= alias $s1)]
+                                      s)))))
+    (cond
+     ((seq-empty-p matches)
+      nil)
+     ((= 1 (length matches))
+      (org-roam-populate (org-roam-node-create :id (caar matches))))
+     ((> (length matches) 1)
+      (org-roam-node-read s nil nil t "Resolve conflict: "))
+     (t
+      (user-error "Multiple nodes exist with title or alias \"%s\"" s)))))
+
+(advice-add 'org-roam-node-from-title-or-alias
+            :override #'+org-roam-node-from-title-or-alias)
