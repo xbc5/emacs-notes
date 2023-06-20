f0fcb0a68de421853fc31a35d6a831fcfaefeef1
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-06-21 00:29:56 +0100

    fix: stop showing IDs in roam buffer (#14)
    Unpin Org-Roam because there's a fix for it in a commit that Doom does
    not yet support. Also unpin `org-roam-bibtex` as per their
    documentation.
    
    Ths fixes #14.
    
    Squashed commit of the following:
    
    commit 9cce33654fd5184ee61523c2fdf8709abc1c4407
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Wed Jun 21 00:27:31 2023 +0100
    
        tidy
    
    commit d9fd5b16d65757e03e75916f5fc6d5a422b06dea
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Wed Jun 21 00:26:59 2023 +0100
    
        add reminder for unpinning
    
    commit fef18b4661f0b1d78a8730dbcabb6cacb0d864a0
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Wed Jun 21 00:26:49 2023 +0100
    
        load bibtex after roam
    
    commit e384d048ec9263fc574726f4429618967805c39e
    Author: xbc5 <87829033+xbc5@users.noreply.github.com>
    Date:   Wed Jun 21 00:19:21 2023 +0100
    
        unpin roam

--- packages.el
+++ packages.el
@@ -6,12 +6,18 @@
   (package! org-ql)
   (package! plantuml-mode))
 
+;; when re-pinning to module, remember to stop using the
+;; org-roam-bibtex recipe, use the package from MELPA intead.
+(unpin! org-roam)
+
 (when (package! org-roam)
   (package! org-roam-server :disable t) ; not yet supported for roam v2
   (when (package! org-ref) ; we don't want ref is no roam
     (when (package! helm-bibtex) ; we don't need if no ref
       (package! bibtex-completion)
-      (package! org-roam-bibtex)))
+      ;; we must use a recipe if we unpin org-roam
+      (package! org-roam-bibtex
+        :recipe (:host github :repo "org-roam/org-roam-bibtex"))))
 
   ;; these deps are a mess, I am not going to even try
   (package! org-roam-dblocks ; deps on: org-tags-filter; plisty
