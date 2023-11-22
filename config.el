a380af3291021af372ec41aed1df36a4ff8b9f4c
xbc5 <87829033+xbc5@users.noreply.github.com>
2023-11-22 09:01:48 +0000

    refactor: tidy config module, group utils

--- config.el
+++ config.el
@@ -1,21 +1,24 @@
-;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 (require 'ht)
 (require 'vulpea)
 (require 'f)
 
-;; misc
+;; util
 (load! "conf/global")
 (load! "lib/xht")
-(load! "secrets/secrets-1")
-(load! "lib/shims")
+(load! "lib/fs")
+(load! "lib/util")
 (load! "lib/xtype")
 (load! "lib/xstr")
-(load! "lib/xnum")
 (load! "lib/xseq")
-(load! "lib/util")
+(load! "lib/xnum")
+
+; ;; misc
+(load! "secrets/secrets-1")
+
+(load! "lib/shims")
 (load! "lib/xtag")
 (load! "lib/smenu")
-(load! "lib/fs")
 (load! "lib/net")
 (load! "lib/prompt")
 (load! "lib/error")
@@ -40,7 +43,7 @@
 (load! "conf/org-ql")
 (load! "conf/org-fancy-priorities")
 (load! "conf/nursery")
-;(load! "conf/org-roam-server")
+; (load! "conf/org-roam-server")
 
 ; WARN: slows down boot by ~6s
 ; (require 'org-roam)
