504b6e971daf00310d2ede84282e63d6d642c24e
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-06-19 22:12:14 +0100

    remove duplicate options from org.el

--- conf/org.el
+++ conf/org.el
@@ -13,14 +13,9 @@ autosync the database."
             (lambda () 
               (xname-change (concat (org-get-title) ".org"))) nil 'local))
 
-;; org is a module, and we're reconfiguring it here (i.e. don't use use-package!)
 (after! org
   ;;(add-hook 'org-mode-hook #'xorg--rename-org-files)
   (add-to-list 'org-modules 'ol-info) ;; for 'info:' links
   (setq org-startup-folded t
         org-image-actual-width (list 800) ; default img width; use a list to enable ATTR fallbacks
-        org-cycle-max-level 2
-        org-refile-targets '((nil :maxlevel . 3)
-                             (("*.org") :maxlevel . 3)) ; refile to other files
-        org-outline-path-complete-in-steps nil ; refile in a single go
-        org-refile-use-outline-path t)) ; show file path for refiling
+        org-cycle-max-level 2))
