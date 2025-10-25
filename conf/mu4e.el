f60b539f452c0f34e028178fb42be208eb578a55
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-10-25 14:05:46 +0100

    set global options

--- conf/mu4e.el
+++ conf/mu4e.el
@@ -1,7 +1,11 @@
 ;;; conf/mu4e.el -*- lexical-binding: t; -*-
 
+;; Loads untracked private configuration values.
 (my/email--load-config-file)
 
 (after! mu4e
-  ;; Auto-updates
-  (setq mu4e-update-interval 60))
+        ;; Auto-updates
+        (setq mu4e-update-interval 30
+              ;; Make "send mail" async.
+              send-mail-function . async-smtpmail-send-it
+              message-send-mail-function . async-smtpmail-send-it))
