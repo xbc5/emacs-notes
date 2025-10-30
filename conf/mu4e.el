09fca93c75c85cba56e1d2e52ae8e99c94937271
xbc5 <87829033+xbc5@users.noreply.github.com>
2025-10-30 10:51:49 +0000

    split mailbox 50/50 with headers

--- conf/mu4e.el
+++ conf/mu4e.el
@@ -5,5 +5,13 @@
 
 (after! mu4e
         ;; Auto-updates
-        (setq mu4e-update-interval 30
-              mu4e-split-view 'vertical))
+        (setq mu4e-update-interval 30)
+
+        ;; Make the mu4e buffer split 50/50 using Emacs display-buffer-alist rules.
+        ;; It splits the the mail "view-buffer". Note that `mu4e-split-view 'vertical`
+        ;; will override this behaviour and cause the split to take up 80% of the window width.
+        (add-to-list 'display-buffer-alist
+                     `(,(regexp-quote mu4e-view-buffer-name)
+                        display-buffer-in-side-window
+                        (side . right)
+                        (window-width . 0.5))))
