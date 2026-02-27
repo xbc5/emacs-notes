d5294bdf846e9a6ff7ec6df852ed779203c4cde7
xbc5 <87829033+xbc5@users.noreply.github.com>
2026-02-27 14:12:43 +0000

    feat(org-node): Create capture templates

--- conf/org-node.el
+++ conf/org-node.el
@@ -11,12 +11,38 @@
         org-mem-watch-dirs (list org-directory))
   (org-mem-updater-mode))
 
+(defun my/org-capture-template(&optional cursor-placeholder)
+  "Produce an Org-compatible capture template.
+CURSOR-PLACEHOLDER: Include a %? under the details heading."
+  (let ((template (list "* summary"
+                        "* index"
+                        "* conclusion"
+                        "* details"
+                        "* ideas"
+                        "* thoughts")))
+    (when cursor-placeholder
+      (push "%?" (nthcdr 4 template)))
+    (s-join "\n" template)))
+
+(defun my/org-capture-template-with-placeholder ()
+  (my/org-capture-template t))
+
 ;; Configure org-node with org-roam-compatible defaults, because notes are in
 ;; roam format, so we preserve slug style, timestamps, and capture behaviour.
 (after! org-node
-  (setq org-node-creation-fn         #'org-node-new-via-roam-capture
-        org-node-file-slug-fn        #'org-node-slugify-like-roam-default
+  (setq org-node-creation-fn         #'org-capture
         org-node-file-timestamp-format "%Y%m%d%H%M%S-")
   (org-node-cache-mode)
   ;; Reuse the org-roam backlinks buffer, since notes are already in roam format.
-  (org-node-roam-accelerator-mode))
+  (org-node-roam-accelerator-mode)
+  (setq org-capture-templates
+        '(("n" "Note"
+           entry (function org-node-capture-target)
+           (function my/org-capture-template-with-placeholder)
+           :empty-lines 1)
+
+          ("q" "Quick"
+           plain (function org-node-capture-target)
+           (function my/org-capture-template)
+           :empty-lines 1
+           :immediate-finish t))))
