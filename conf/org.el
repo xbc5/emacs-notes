bfcf141630345ec22c9ec9806c4b92768c89d5b1
xbc5 <87829033+xbc5@users.noreply.github.com>
2026-03-02 18:40:44 +0000

    refactor(org): Use use-package

--- conf/org.el
+++ conf/org.el
@@ -1,10 +1,10 @@
 ;; -*- lexical-binding: t; -*-
-(make-directory org-directory t)
-
-(map! "M-M" #'org-capture)
-
-(after! org
-  (add-to-list 'org-modules 'ol-info) ;; for 'info:' links
+(use-package! org
+  :init
+  (make-directory org-directory t)
+  (map! "M-M" #'org-capture)
+  :config
+  (add-to-list 'org-modules 'ol-info) ; Enable 'info:' links.
   (setq org-startup-folded t
-        org-image-actual-width (list 800) ; default img width; use a list to enable ATTR fallbacks
+        org-image-actual-width (list 800) ; Use a list to enable ATTR fallbacks.
         org-cycle-max-level 2))
