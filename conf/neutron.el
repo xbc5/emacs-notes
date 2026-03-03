2ca1ca857eac14d428472a1a55c144e8eed8a039
xbc5 <87829033+xbc5@users.noreply.github.com>
2026-03-03 21:39:21 +0000

    feat(neutron): Map project status setter

--- conf/neutron.el
+++ conf/neutron.el
@@ -2,18 +2,17 @@
 (require 'neutron)
 (neutron--setup-todo-keywords)
 
-
 (map! "M-t" (cmd! (neutron-create-task t))
       :leader
       :prefix "j"
-      :desc "Create habit"        "h" #'neutron-create-habit
-      :desc "Create project"      "c" #'neutron-create-project
-      :desc "Create sibling"      "s" #'neutron-create-sibling
-      :desc "Set project status"  "l" #'neutron-set-project-status
-      :desc "Set project status"  "L" (cmd! (neutron-set-project-status t))
-      :desc "Create global task"  "T" #'neutron-create-task
-      :desc "Create local task"   "t" (cmd! (neutron-create-task t))
-      :desc "Delete project"      "d" #'neutron-delete-project
-      :desc "Move project"        "m" #'neutron-move-project
-      :desc "Refile tasks"        "k" #'neutron-refile-tasks
-      :desc "Rename project"      "R" #'neutron-rename-project)
+      :desc "Create global task"         "T" #'neutron-create-task
+      :desc "Create habit"               "h" #'neutron-create-habit
+      :desc "Create local task"          "t" (cmd! (neutron-create-task t))
+      :desc "Create project"             "c" #'neutron-create-project
+      :desc "Create sibling"             "s" #'neutron-create-sibling
+      :desc "Delete project"             "d" #'neutron-delete-project
+      :desc "Move project"               "m" #'neutron-move-project
+      :desc "Refile tasks"               "k" #'neutron-refile-tasks
+      :desc "Rename project"             "R" #'neutron-rename-project
+      :desc "Set global project status"  "L" (cmd! (neutron-set-project-status t))
+      :desc "Set local project status"   "l" #'neutron-set-project-status)
